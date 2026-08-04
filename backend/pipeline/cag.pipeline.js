b6fda32c7bd04916098522a8652e46ccbf4da050
QUDDUS <qudduslarek@proton.me>
2026-08-04 23:29:01 +0500

    CAG pipeline creation

---
+++ backend/pipeline/cag.pipeline.js
@@ -0,0 +1,41 @@
+import { ChatGroq } from "@langchain/groq";
+import { StateGraph, Annotation, END, START } from "@langchain/langgraph";
+import {
+  SystemMessage,
+  HumanMessage,
+  BaseMessage,
+} from "@langchain/core/message";
+
+const LLM = new ChatGroq({
+  apiKey: process.env.GROQ_API_KEY,
+  model: "llama-3.3-70b-versatile",
+  temperature: 0,
+});
+
+const GraphState = Annotation.Root({
+  corpus: Annotation(),
+  message: Annotation({
+    reducer: (x, y) => x.concat(y),
+    default: () => [],
+  }),
+});
+
+async function callModelNode(state) {
+  const PROPMT = new SystemMessage(
+    `You are an enterprise AI assistant. Answer queries using strictly the provided reference context.\n\n` +
+      `================ KNOWLEDGE BASE BASELINE ================\n` +
+      `${state.corpus}\n` +
+      `=========================================================`,
+  );
+
+  const fullMessage = [PROPMT, ...state.messages];
+  const response = await LLM.invoke(fullMessage);
+
+  return {
+    messages: [response],
+  }
+}
+
+const workflow = new StateGraph(GraphState).addNode("agent", callModelNode).addNode(START,"agent").addEdge("agent", END);
+
+export const CAGProcess = workflow.compile();
