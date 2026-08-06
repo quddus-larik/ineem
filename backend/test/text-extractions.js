9bbe2c0f5390b1010bf4c605a57903ef641c3035
QUDDUS <qudduslarek@proton.me>
2026-08-06 19:01:19 +0500

    remove unwanted files

--- backend/test/text-extractions.js
+++
@@ -1,7 +0,0 @@
-import { extractPdfTextWithOcr } from "../layers/text-extraction";
-
-async function Run() {
-    let data = await extractPdfTextWithOcr("../uploads/1785860236802.pdf");
-
-    return data;
-}
