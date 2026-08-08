7f54de68b6a955fc28223184e792ced167796aab
QUDDUS <qudduslarek@proton.me>
2026-08-08 21:53:17 +0500

    reset all files

--- metro.config.js
+++
@@ -1,8 +0,0 @@
-const { getDefaultConfig } = require("expo/metro-config");
-const { withUniwindConfig } = require("uniwind/metro");
-
-const config = getDefaultConfig(__dirname);
-
-module.exports = withUniwindConfig(config, {
-  cssEntryFile: "./src/global.css",
-});
