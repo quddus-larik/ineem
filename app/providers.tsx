02d10ba79c0a60582476ab3f185b192ae911384f
QUDDUS <qudduslarek@proton.me>
2026-08-19 17:36:32 +0500

    chore: fetch repos on first load

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,19 +1,27 @@
 "use client"
 
 // app/providers.tsx
+import { useEffect } from "react";
 import { ThemeProvider } from "next-themes";
 import { ProgressProvider } from "@bprogress/next/app";
+import { useGithubRepoStore } from "@/stores/github.repos";
 
 export function Providers({ children }: { children: React.ReactNode }) {
-    return (
-        <ThemeProvider attribute="class" defaultTheme="dark">
-            <ProgressProvider
-                height="4px"
-                color="#5465ff"
-                options={{ showSpinner: false }}
-            >
-                {children}
-            </ProgressProvider>
-        </ThemeProvider>
-    );
+  const fetchRepos = useGithubRepoStore((s) => s.fetchRepos);
+
+  useEffect(() => {
+    fetchRepos();
+  }, [fetchRepos]);
+
+  return (
+    <ThemeProvider attribute="class" defaultTheme="dark">
+      <ProgressProvider
+        height="4px"
+        color="#5465ff"
+        options={{ showSpinner: false }}
+      >
+        {children}
+      </ProgressProvider>
+    </ThemeProvider>
+  );
 }
