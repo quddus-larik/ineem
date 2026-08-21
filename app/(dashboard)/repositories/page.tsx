25e0ae8ef1f86850bce010640480e3f39ea63230
QUDDUS <qudduslarek@proton.me>
2026-08-21 13:26:46 +0500

    chore: update UI for repositories

--- app/(dashboard)/repositories/page.tsx
+++ app/(dashboard)/repositories/page.tsx
@@ -1,7 +1,9 @@
 "use client";
 
-import { Table, Label, Description } from "@heroui/react";
+import { Table, Label, Description, Card, Chip, Separator } from "@heroui/react";
+import { GitCommit, Lock, LockOpen } from "@mynaui/icons-react";
 import { useGithubRepoStore } from "@/stores/github.repos";
+import { redirect } from "next/navigation";
 
 export default function Page() {
   const { repos, loadingRepos, repoError } = useGithubRepoStore();
@@ -11,38 +13,41 @@ export default function Page() {
   }
 
   if (repoError) {
-    return <div>Error: {repoError}</div>;
+    redirect("/");
   }
 
   return (
     <div>
-      <Table>
-        <Table.ScrollContainer>
-          <Table.Content aria-label="Team members">
-            <Table.Header>
-              <Table.Column>Name</Table.Column>
-              <Table.Column>Last Update</Table.Column>
-              <Table.Column>Language</Table.Column>
-              <Table.Column>Visibility</Table.Column>
-            </Table.Header>
-            <Table.Body>
-              {repos.map((repo) => (
-                <Table.Row key={repo.id}>
-                  <Table.Cell>
-                    <div>
-                      <Label>{repo.name}</Label>
-                      <Description className={"line-clamp-2 w-md"}>{repo.description}</Description>
-                    </div>
-                  </Table.Cell>
-                  <Table.Cell>{repo.updated_at}</Table.Cell>
-                  <Table.Cell>{repo.language}</Table.Cell>
-                  <Table.Cell>{repo.visibility}</Table.Cell>
-                </Table.Row>
-              ))}
-            </Table.Body>
-          </Table.Content>
-        </Table.ScrollContainer>
-      </Table>
+      <div>
+        <p classNam={"text-4xl font-semibold"}>Repositories</p>
+        <p className="text-base">Desc</p>
+      </div>
+      <div className={"grid grid-cols-4 gap-2"}>
+        {repos.length > 0 &&
+          repos.map((itm, idx) => (
+            <Card className={"hover-ring"}>
+              <Card.Header>
+                <Card.Title>{itm.title}</Card.Title>
+                <Card.Description className={"line-clamp-1"}>
+                  {itm.description ?? "--"}
+                </Card.Description>
+              </Card.Header>
+              <Card.Footer>
+                <div className={"flex items-center gap-2"}>
+                  {itm.visibility == "PUBLIC" ? <LockOpen size={16} /> : <Lock size={16} />}
+                  <Chip color={"accent"} variant={"soft"} size={"sm"}>
+                    {itm.language}
+                  </Chip>
+                  <Separator orientation={"vertical"} />
+                  <Chip color={"accent"} variant={"secondary"} size={"sm"}>
+                    <GitCommit size={15} />{" "}
+                    <Chip.Label>{itm.commit_count}</Chip.Label>
+                  </Chip>
+                </div>
+              </Card.Footer>
+            </Card>
+          ))}
+      </div>
     </div>
   );
 }
